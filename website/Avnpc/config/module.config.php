956f7ed75d8eca1e22ff504925349fe5f0c6b7bb
AlloVince <allo.vince@gmail.com>
2012-08-22 21:29:35 +0800

    life Controller

--- website/Avnpc/config/module.config.php
+++ website/Avnpc/config/module.config.php
@@ -27,12 +27,27 @@ return array(
                 ),
                 'priority' => 2,
             ),
+            'frontlife' => array(
+                'type' => 'Zend\Mvc\Router\Http\Segment',
+                'options' => array(
+                    'route' => '/life[/:id]',
+                    'constraints' => array(
+                        'id'     => '[a-zA-Z][a-zA-Z0-9_-]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Avnpc\Controller\LifeController',
+                        'action' => 'index',
+                    ),
+                ),
+                'priority' => 2,
+            ),
         ),
     ),
     'controllers' => array(
         'invokables' => array(
             'Avnpc\Controller\IndexController' => 'Avnpc\Controller\IndexController',
             'Avnpc\Controller\PagesController' => 'Avnpc\Controller\PagesController',
+            'Avnpc\Controller\LifeController' => 'Avnpc\Controller\LifeController',
         ),
     ),
 
@@ -41,13 +56,9 @@ return array(
             'layout/layout' => __DIR__ . '/../layout/avnpc.phtml',
             'avnpc/index' => __DIR__ . '/../view/avnpc/index.phtml',
             'avnpc/pages/get' => __DIR__ . '/../view/avnpc/pages/get.phtml',
-            'avnpc/category/get' => __DIR__ . '/../view/avnpc/category/get.phtml',
-            'avnpc/order/index' => __DIR__ . '/../view/avnpc/order/index.phtml',
+            'avnpc/life/index' => __DIR__ . '/../view/avnpc/life/index.phtml',
+            'error/index'   => __DIR__ . '/../view/error/index.phtml',
+            'error/404'   => __DIR__ . '/../view/error/index.phtml',
         ),
-
     ),
-
-    'page_components' => array(
-    ),
-
 );
